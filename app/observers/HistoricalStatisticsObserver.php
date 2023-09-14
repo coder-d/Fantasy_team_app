018267a1d6a084bfdb76a239f4241fd96116adf3
Debanjan <debanjan@lamium.fi>
2023-09-14 22:34:28 +0530

    Added empty constructors for observers

--- app/observers/HistoricalStatisticsObserver.php
+++ app/observers/HistoricalStatisticsObserver.php
@@ -4,5 +4,10 @@ namespace FantasyTeamApp\Observers;
 
 class HistoricalStatisticsObserver
 {
+    public function __construct(array $players = [])
+    {
+        // Initialize the observer with player data if needed
+    }
+
     // Implement observer logic here
 }
--- app/observers/RecentPerformanceObserver.php
+++ app/observers/RecentPerformanceObserver.php
@@ -4,5 +4,12 @@ namespace FantasyTeamApp\Observers;
 
 class RecentPerformanceObserver
 {
+    public function __construct(array $players = [])
+    {
+        // Initialize the observer with player data
+        // ...
+    }
+
     // Implement observer logic here
 }
+
