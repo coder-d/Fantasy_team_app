c800374c21a9ca1ad219164c411771588bbe33eb
Debanjan <debanjan@lamium.fi>
2023-09-16 21:28:32 +0530

    Add /createTeam route, update TeamView.php to show created team, write a test case for TeamView render()

--- app/index.php
+++ app/index.php
@@ -1,1 +1,43 @@
+<<<<<<< Updated upstream
 This is the index page.
+=======
+<?php
+
+// Require the autoloader to autoload classes
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use FantasyTeamApp\Core\Router;
+
+// Create a new router instance
+$router = new FantasyTeamApp\Core\Router();
+
+// Define routes
+$router->addRoute('GET', '^/$', function () {
+    // Handle the home page
+    echo 'Welcome to the home page!';
+});
+
+$router->addRoute('GET', 'teams', function () {
+    // Handle the teams page
+    echo 'View all teams';
+});
+
+$router->addRoute('GET', '/teams/(\d+)', function ($teamId) {
+    // Handle viewing a specific team using $teamId
+    echo 'View team with ID: ' . $teamId;
+});
+
+$router->addRoute('POST', '/createTeam', function () {
+    // Handle the creation of a team
+    // instantiate the TeamController and call the createTeam method here
+    $teamController = new FantasyTeamApp\Controllers\TeamController(new FantasyTeamApp\Views\TeamView());
+    $gameType = $_POST['gameType']; // Assuming gameType is submitted in the form
+    $players = $_POST['players']; // Assuming players are submitted in the form
+    $teamController->createTeam($gameType, $players);
+});
+
+// Add more routes as needed
+
+// Route the current request
+$router->route();
+>>>>>>> Stashed changes
--- app/src/Views/TeamView.php
+++ app/src/Views/TeamView.php
@@ -4,5 +4,31 @@ namespace FantasyTeamApp\Views;
 
 class TeamView
 {
+<<<<<<< Updated upstream
     // Implement view rendering logic here
 }
+=======
+    public function render(array $team)
+    {
+        // You can use PHP to generate HTML content here
+        echo '<h1>Fantasy Team Selection</h1>';
+        
+        // Check if the team is empty
+        if (empty($team)) {
+            echo '<p>No team data available.</p>';
+        } else {
+            echo '<h2>Selected Team</h2>';
+            echo '<ul>';
+            foreach ($team['team'] as $player) {
+                echo '<li>' . $player['name'] . '</li>';
+            }
+            echo '</ul>';
+        }
+
+        echo '<form action="/submit" method="post">';
+        // Add form fields, dynamic data, and logic here
+        echo '<input type="submit" value="Submit Team">';
+        echo '</form>';
+    }
+}
+>>>>>>> Stashed changes
