a681b15ac9828ea98c9ee28d3921eb7c6b7732e3
Debanjan <debanjan@lamium.fi>
2023-09-18 17:29:04 +0530

    Added bootstrap 4 via CDN, added bootstrap classes to createForm

--- app/src/Views/TeamCreationView.php
+++ app/src/Views/TeamCreationView.php
@@ -10,19 +10,28 @@ class TeamCreationView
         ob_start();
         ?>
 
-        <h1>Create Your Fantasy Team</h1>
-        <form id="team-creation-form" method="POST">
-            <!-- Form fields go here -->
-            <label for="team-name">Team Name:</label>
-            <input type="text" id="team-name" name="team-name" required>
-            <!-- Add more form fields and validation as needed -->
-            <button type="submit">Create Team</button>
-        </form>
-
-        <!-- Include your TypeScript file here -->
-        <script src="../public/js/team-creation-form.js"></script>
-        
-        <?php
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <h1 class="text-center">Create Your Fantasy Team</h1>
+                <form id="team-creation-form" class="needs-validation" novalidate>
+                    <!-- Form fields go here -->
+                    <div class="form-group">
+                        <label for="team-name">Team Name:</label>
+                        <input type="text" class="form-control" id="team-name" name="team-name" required>
+                        <div class="invalid-feedback">
+                            Team Name is required.
+                        </div>
+                    </div>
+                    <!-- Add more form fields and validation as needed -->
+                    <button type="submit" class="btn btn-primary btn-block">Create Team</button>
+                </form>
+                <!-- Include your TypeScript file here -->
+                <script src="team-creation-form.js"></script>
+            </div>
+        </div>
+    </div> 
+    <?php
         // End output buffering and capture the content
         $content = ob_get_clean();
 
